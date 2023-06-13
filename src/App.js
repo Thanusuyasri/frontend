6c67f1fa68a43a782525e194c707fac6331b3261
Thanusuyasri <82963993+Thanusuyasri@users.noreply.github.com>
2023-06-13 18:48:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Loginpage,{Lo} from './Loginpage';
+import Adminlogin,{Ad} from './Adminlogin';
+import SignUp,{Sn} from './SignUp';
+import Admindeleteflight,{De} from './Admindeleteflight';
+import Adminaddflight,{Add} from './Adminaddflight';
+import Usersearchflight from './Usersearchflight';
+import UserBookingflight,{Ub} from './UserBookingflight';
+import Frontpage from './Frontpage';
+import Selectallflightdata from './Selectallflightdata';
 import logo from './logo.svg';
+import Adminviewbooking from './Adminviewbooking';
+import Logoutpage from './Logoutpage';
 import './App.css';
+import Usermybooking from './Usermybooking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      
+      
+      <Routes>
+      <Route exact path="/" element={<Frontpage/>} />
+        <Route path="/login" element={<Lo />} />
+        <Route path="/signup" element={<Sn />} />
+        <Route path="/adminlogin" element={<Ad />} />
+        <Route path="/booking" element={<Ub />} />
+        <Route path="/userflight" element={<Usersearchflight />} />
+        <Route path="/Admindeleteflight" element={<De />} />
+        <Route path="/Adminaddflight" element={<Add />} />
+        <Route path="/Adminviewbooking" element={<Adminviewbooking />} />
+        <Route path="/Mybooking" element={<Usermybooking />} />
+        <Route path="/Logout" element={<Logoutpage />} />
+        <Route path="/allflight" element={<Selectallflightdata />} />
+      </Routes>
+      {/* <Usersearchflight/> */}
+    </Router>
     </div>
+
   );
 }
 
